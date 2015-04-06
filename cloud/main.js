8227ba9a1dc2d5520f1e136ab6f848b0227d3649
Alexander Tu <alextu87@gmail.com>
2015-04-05 17:54:17 -0700

    resturant --> restaurant

--- cloud/main.js
+++ cloud/main.js
@@ -35,9 +35,9 @@ Parse.Cloud.job("triggerChatPushNotify", function(request, response){
                 for(var matchKey in matches){
                     var match = matches[matchKey];
                     //console.log(match);
-                    sendPushNotificaiton(match.get('reqUserId'), match.id, 2, match.get('matchedUsername'), match.get('resturantName') );
+                    sendPushNotificaiton(match.get('reqUserId'), match.id, 2, match.get('matchedUsername'), match.get('restaurantName') );
                     setUserToMatched(match.get('reqUserId'),match.id, match.get('matchedUserID'));
-                    sendPushNotificaiton(match.get('matchedUserID'), match.id, 2, match.get('matchedUsername'), match.get('resturantName') );
+                    sendPushNotificaiton(match.get('matchedUserID'), match.id, 2, match.get('matchedUsername'), match.get('restaurantName') );
                     setUserToMatched(match.get('matchedUserID'),match.id, match.get('reqUserId'));
 
                 }
@@ -87,8 +87,8 @@ Parse.Cloud.job("triggerMatchPushNotify", function(request, response) {
                                     userlist.push(match.get('reqUserId'));
                                     userlist.push(match.get('matchedUserID'));
 
-                                    sendPushNotificaiton(match.get('reqUserId'), match.id, 1, match.get('matchedUsername'), match.get('resturantName') );
-                                    sendPushNotificaiton(match.get('matchedUserID'), match.id, 1, match.get('matchedUsername'), match.get('resturantName') );
+                                    sendPushNotificaiton(match.get('reqUserId'), match.id, 1, match.get('matchedUsername'), match.get('restaurantName') );
+                                    sendPushNotificaiton(match.get('matchedUserID'), match.id, 1, match.get('matchedUsername'), match.get('restaurantName') );
                                 }
                             }
                         }
@@ -106,14 +106,14 @@ Parse.Cloud.job("triggerMatchPushNotify", function(request, response) {
 });
 
 
-function sendPushNotificaiton(userid, matchid, action, matchedUserName, resturantName){
+function sendPushNotificaiton(userid, matchid, action, matchedUserName, restaurantName){
     var query = new Parse.Query(Parse.Installation);
     query.equalTo('userid', userid);
     var title = "We've found a potential match!";
-    var alert = "Have lunch with "+ matchedUserName +" at "+ resturantName +" ?";
+    var alert = "Have lunch with "+ matchedUserName +" at "+ restaurantName +" ?";
     if(action == 2){
         title = "We've matched you up!";
-        alert = "Get in touch with "+ matchedUserName +" and have a great lunch at "+ resturantName +" !";
+        alert = "Get in touch with "+ matchedUserName +" and have a great lunch at "+ restaurantName +" !";
     }
 
 
