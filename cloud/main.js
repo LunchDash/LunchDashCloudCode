31c4c700dd9c8e68ba77d899c298060fdb4ee22c
Alexander Tu <alextu87@gmail.com>
2015-04-08 02:19:19 -0700

    Combined both push notification jobs into a single job. Added functions for both push notifications. Added a check to see if the users's status is "Matched".  This only happens after the push notification gets sent out, so we know not to send another push notif

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,3 @@
-
 Parse.Cloud.define("pushNotfication", function(request, response) {
     var userid = request.params.userid;
     sendPushNotificaiton(userid, '');
@@ -24,25 +23,101 @@ Parse.Cloud.define("userGetInfo", function(request, response) {
     }
 });
 
+//Define functions and job for push notifications.
 
-Parse.Cloud.job("triggerChatPushNotify", function(request, response){
-    var query = new Parse.Query("UserRestaurantMatchesTable");
-    query.equalTo('reqStatus',"accepted");
-    query.equalTo('matchedStatus',"accepted");
-    query.find({
-        success: function(matches){
-            if(matches.length > 0) {
-                for(var matchKey in matches){
-                    var match = matches[matchKey];
+Parse.Cloud.define("triggerChatPushNotify",function(request,response){
+    triggerChatPushNotify(request,response);
+});
+
+Parse.Cloud.define("triggerMatchPushNotify", function(request,response){
+    triggerMatchPushNotify(request,response);
+});
+
+Parse.Cloud.job("pushNotify", function(request,response){
+    triggerMatchPushNotify(request,response);
+    triggerChatPushNotify(request,response);
+});
+
+function triggerMatchPushNotify(request,response){
+  var query = new Parse.Query("UserTable");
+  query.equalTo('status', "Waiting");
+  query.find({
+    success: function(results) {
+
+        var userlist = new Array();
+
+        for (var i = 0; i < results.length; i++) {
+            var userid = results[i].get('userId');
+
+            var queryreq = new Parse.Query("UserRestaurantMatchesTable")
+            .equalTo('reqUserId', userid)
+            .equalTo('reqStatus', 'waiting')
+            .equalTo('matchedStatus', 'waiting');
+            var querymatch = new Parse.Query("UserRestaurantMatchesTable")
+            .equalTo('matcherUserId', userid)
+            .equalTo('matchedStatus', 'waiting')
+            .equalTo('reqStatus', 'waiting');
+
+            Parse.Query.or(queryreq,querymatch).find({
+                success: function(matches) {
+                    if(matches.length > 0){
+                        for(var matchKey in matches){
+                            var match = matches[matchKey];
+                            if((userlist.indexOf(match.get('reqUserId')) == -1)
+                                && (userlist.indexOf(match.get('matchedUserID')) == -1)) {
+                                    console.log('Found match: '+match.get('matchedUsername')+' and '+match.get('reqUserId')); //Log
+
+                                userlist.push(match.get('reqUserId'));
+                                userlist.push(match.get('matchedUserID'));
+
+                                sendPushNotificaiton(match.get('reqUserId'), match.id, 1, match.get('matchedUsername'), match.get('restaurantName') );
+                                sendPushNotificaiton(match.get('matchedUserID'), match.id, 1, match.get('matchedUsername'), match.get('restaurantName') );
+                            }
+                        }
+                    }
+                },
+                error: function() {
+                    response.error("Could not find user");
+                }
+            });
+}
+},
+error: function() {
+    response.error("Match push failed.");
+}
+});
+}
+
+function triggerChatPushNotify(request,response){
+ var query = new Parse.Query("UserRestaurantMatchesTable");
+ query.equalTo('reqStatus',"accepted");
+ query.equalTo('matchedStatus',"accepted");
+ query.find({
+    success: function(matches){
+        if(matches.length > 0) {
+            for(var matchKey in matches){
+                var match = matches[matchKey];
                     //console.log(match);
                     var reqUserId = match.get('reqUserId');
                     var matchedUserId = match.get('matchedUserID');
-                    sendPushNotificaiton(reqUserId, match.id, 2, match.get('matchedUsername'), match.get('restaurantName') );
-                    sendPushNotificaiton(matchedUserId, match.id, 2, match.get('matchedUsername'), match.get('restaurantName') );
+
+                    var query2 = new Parse.Query("UserTable");
+                    query2.equalTo('userId', reqUserId);
+                    query2.find({
+                        success: function(results) {
+                            var status = results[0].get("status");
+                            if(status != "Matched"){
+                                sendPushNotificaiton(reqUserId, match.id, 2, match.get('matchedUsername'), match.get('restaurantName'));
+                                sendPushNotificaiton(matchedUserId, match.id, 2, match.get('matchedUsername'), match.get('restaurantName'));
 
                     //Remove both users from the URTable.  This will prevent further matches from being made from them.
                     clearUser(reqUserId);
-                    clearUser(matchedUserId);
+                    clearUser(matchedUserId);    
+                }
+
+            }
+        });
+
                 }
             } else {
                 response.success("No Chat matches yet");
@@ -52,76 +127,22 @@ Parse.Cloud.job("triggerChatPushNotify", function(request, response){
         error: function(){
             response.error("Chat Push Failed.");
         }
-    })
-});
-
-
-
-Parse.Cloud.job("triggerMatchPushNotify", function(request, response) {
-
-    var query = new Parse.Query("UserTable");
-    query.equalTo('status', "Waiting");
-    query.find({
-        success: function(results) {
-
-            var userlist = new Array();
-
-            for (var i = 0; i < results.length; i++) {
-                var userid = results[i].get('userId');
-
-                var queryreq = new Parse.Query("UserRestaurantMatchesTable")
-                    .equalTo('reqUserId', userid)
-                    .equalTo('reqStatus', 'waiting')
-                    .equalTo('matchedStatus', 'waiting');
-                var querymatch = new Parse.Query("UserRestaurantMatchesTable")
-                    .equalTo('matcherUserId', userid)
-                    .equalTo('matchedStatus', 'waiting')
-                    .equalTo('reqStatus', 'waiting');
-
-                Parse.Query.or(queryreq,querymatch).find({
-                    success: function(matches) {
-                        if(matches.length > 0){
-                            for(var matchKey in matches){
-                                var match = matches[matchKey];
-                                if((userlist.indexOf(match.get('reqUserId')) == -1)
-                                    && (userlist.indexOf(match.get('matchedUserID')) == -1)) {
-                                    console.log('Found match: '+match.get('matchedUsername')+' and '+match.get('reqUserId')); //Log
-                                    
-                                    userlist.push(match.get('reqUserId'));
-                                    userlist.push(match.get('matchedUserID'));
-
-                                    sendPushNotificaiton(match.get('reqUserId'), match.id, 1, match.get('matchedUsername'), match.get('restaurantName') );
-                                    sendPushNotificaiton(match.get('matchedUserID'), match.id, 1, match.get('matchedUsername'), match.get('restaurantName') );
-                                }
-                            }
-                        }
-                    },
-                    error: function() {
-                        response.error("Could not find user");
-                    }
-                });
-            }
-        },
-        error: function() {
-            response.error("Match push failed.");
-        }
     });
-});
+}
 
 
 function sendPushNotificaiton(userid, matchid, action, matchedUserName, restaurantName){
     var query = new Parse.Query(Parse.Installation);
     query.equalTo('userid', userid);
     if(action == 1){
-        
+
         var title = "We've found a potential match!";
         var alert = "Have lunch with "+ matchedUserName +" at "+ restaurantName +" ?";
     } else  if(action == 2){
         title = "We've matched you up!";
         alert = "Get in touch with "+ matchedUserName +" and have a great lunch at "+ restaurantName +" !";
     }
-
-
+    
     Parse.Push.send({
         where: query, // Set our Installation query
         data: {
@@ -143,7 +164,6 @@ function sendPushNotificaiton(userid, matchid, action, matchedUserName, restaura
             console.log("Push error ");
         }
     });
-
 }
 
 function setUserToMatching(userid)
@@ -171,7 +191,7 @@ function setUserToMatched(userid, match, matchedUserid)
 }
 
 function clearUser(userId){ //Call this after ChatPushNotifs have been sent out.
-     
+
     //Remove all their entries from UserRestaurantsTable
     var query = new Parse.Query("UserRestaurantsTable");
     query.equalTo('userId',userId);    
